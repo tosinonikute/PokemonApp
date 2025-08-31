c65a6cef9ed1681570aa59a09a48bb4ecde41210
Tosin <tosinmath007@gmail.com>
2025-08-31 22:47:24 +0100

    Create buildScr directory

---
+++ buildSrc/src/main/java/ProjectModules.kt
@@ -0,0 +1,8 @@
+object ProjectModules {
+    const val APP = ":app"
+    const val POKEMON_DATA = ":pokemon-data"
+    const val POKEMON_DATASOURCE = ":pokemon-datasource"
+    const val POKEMON_DOMAIN = ":pokemon-domain"
+    const val POKEMON_PRESENTATION = ":pokemon-presentation"
+    const val POKEMON_UI = ":pokemon-ui"
+}
