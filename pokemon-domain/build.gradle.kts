0ff154013bca1856290e21fddeb5ac9a4347ec98
Tosin <tosinmath007@gmail.com>
2025-08-31 22:36:43 +0100

    Setup pokemon project directories
    * pokemon-datasource
    * pokemon-data
    * pokemon-domain
    * pokemon-presentation
    * pokemon-ui

---
+++ pokemon-domain/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.pokemon.domain"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PokemonApp"
+include(":app")
+include(":pokemon-data")
+include(":pokemon-datasource")
+include(":pokemon-domain")
+include(":pokemon-presentation")
+include(":pokemon-ui")
